3b4098234535db6ad81acdc35de353de6202aa9d
ngwamalvin <106126808+ngwamalvin@users.noreply.github.com>
2024-05-27 19:53:47 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,3 +33,11 @@ module "ec2_prod" {
   iam_role_name = module.iam.ec2_iam_role_name
   key_pair_name = var.key_pair_name
 }
+
+module "ec2_prod_test" {
+  source = "./ec2"
+  name   = "prod"
+  tags          = local.common_tags
+  iam_role_name = module.iam.ec2_iam_role_name
+  key_pair_name = var.key_pair_name
+}
